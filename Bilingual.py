cdcc6c7d5177519b5d190079462ed54989ac6c54
jaosn999999 <1209601741@qq.com>
2023-11-13 22:04:48 +0800

    。

--- Bilingual.py
+++ Bilingual.py
@@ -10,8 +10,8 @@ translator = Translator(proxies='http://127.0.0.1:7890')
 def translate(sentence):
     while True:
         try:
-            # text = translator.translate(sentence)
-            text = baidu_translator.baidutrans(sentence)
+            text = translator.translate(sentence)
+            # text = baidu_translator.baidutrans(sentence)
             break
         except Exception as e:
             print(e)
@@ -33,54 +33,13 @@ def split_srt():
             print("translate google Disconnected")
            
 
-# def split_srt():
-#     # 打开SRT文件
-#     subs = pysrt.open('subtitle.srt')
-    
-#     # 遍历所有字幕并将它们添加到数组中
-#     pre = 0
-#     subtitle_all = ''
-#     for i, sub in enumerate(subs):
-#         subtitle_line = sub.text.strip()
-#         subtitle_all = subtitle_all + ' ' + subtitle_line
-#         if subtitle_line.endswith('.') or subtitle_line.endswith('?') or subtitle_line.endswith('!'):
-#             while True:
-#                 try:
-#                     text = translate(subtitle_all)
-#                     modify_srt(text, pre, i+1-pre)
-#                     pre = i + 1
-#                     subtitle_all = ''
-#                     break
-#                 except Exception as e:
-#                     print("translate google Disconnected")
-#                     time.sleep(5)
-#             time.sleep(2)
-
 
 def modify_srt(subtitle,index):
     subs = pysrt.open('subtitle.srt')
     subs[index].text = subs[index].text.strip()+'\n'+subtitle
+    # subs[index].text = subtitle
     subs.save('subtitle.srt')
 
-
-# def modify_srt(subtitle, start_index, distanct):
-#     subs = pysrt.open('subtitle.srt')
-#     length = distanct+start_index
-#     try:
-#         text_length = len(subtitle)//distanct
-#     except Exception as e:
-#         raise e
-
-#     segments = []
-#     for i in range(0, len(subtitle), text_length):
-#         segments.append(subtitle[i:i+text_length])
-
-#     j = 0
-#     for i in range(start_index, length):
-#         subs[i].text = subs[i].text.strip()+'\n'+segments[j]
-#         j = j+1
-
-#     subs.save('subtitle.srt')
 
 
 # split_srt()
--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 from pytube import YouTube
 import os
 
-
 def download_video(video_link):
     yt = YouTube(video_link)
     print('download...')
     yt.streams.filter().get_highest_resolution().download(filename='overview.mp4')
     print('ok!')
 
-
-download_video('https://www.youtube.com/watch?v=Aryuano1V0o')
+download_video('https://www.youtube.com/watch?v=ROZmiwv5pGU')
